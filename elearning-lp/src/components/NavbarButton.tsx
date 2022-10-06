0fccaac25ddccaa9116049b5a3faa31e8d161310
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-06 22:03:37 +0200

    initialized navbar button with tailwind styles

---
+++ elearning-lp/src/components/NavbarButton.tsx
@@ -0,0 +1,9 @@
+export const GetStartetButton = () => {
+  return (
+    <div>
+      <button className="bg-black text-white rounded-full py-2 px-6">
+        Get startet
+      </button>
+    </div>
+  );
+};
