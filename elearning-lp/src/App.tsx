1c46af01d13cf217b8504196e9d8fc5bd5ae7133
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-06 22:03:58 +0200

    initialized navbar component (wip)

--- elearning-lp/src/App.tsx
+++ elearning-lp/src/App.tsx
@@ -1,14 +1,12 @@
-
-import './App.css'
+import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-
-
   return (
     <div className="App">
-     
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
