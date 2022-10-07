77203f5c98af9503b4f9660d2814b6d7294a0ec7
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-07 07:58:25 +0200

    added maincontent and footer in app

--- elearning-lp/src/App.tsx
+++ elearning-lp/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Footer } from "./components/Footer/Footer";
+import { MainContent } from "./components/MainContent/MainContent";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gradient-to-b from-white to-gradient-background-blue">
       <Navbar />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
