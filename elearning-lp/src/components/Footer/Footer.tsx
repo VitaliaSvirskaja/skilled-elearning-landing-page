772def42352ff70444900a204fc8da3a9f7fa148
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-07 07:58:03 +0200

    added container for footer for correct hight and bg-color

---
+++ elearning-lp/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import logoWhite from "../../assets/logo-light.svg";
+import { FooterButton } from "./FooterButton";
+
+export const Footer = () => {
+  return (
+    <div className="bg-darkblue h-32 flex flex-col justify-center">
+      <div className="flex justify-between items-center bg-darkblue mx-4">
+        <img src={logoWhite} alt="logo-navbar" className="w-24" />
+        <FooterButton />
+      </div>
+    </div>
+  );
+};
---
+++ elearning-lp/src/components/Footer/FooterButton.tsx
@@ -0,0 +1,9 @@
+export const FooterButton = () => {
+  return (
+    <div>
+      <button className="bg-gradient-to-t from-gradient-orange-dark to-gradient-violet-dark text-white rounded-full py-2 px-6">
+        Get startet
+      </button>
+    </div>
+  );
+};
