92eca808e125c2f7c17a26473fc1c007fd547dce
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-07 19:20:43 +0200

    implemented mobile herosection (wip)

--- elearning-lp/src/components/MainContent/Herosection.tsx
+++ elearning-lp/src/components/MainContent/Herosection.tsx
@@ -1,5 +1,16 @@
-interface Props {}
+import { HerosectionButton } from "./HerosectionButton";
+import heroImageMobile from "../../assets/image-hero-mobile@2x.webp";
 
-export const Herosection = (props: Props) => {
-  return <div>Herosection</div>;
+export const Herosection = () => {
+  return (
+    <div className="flex flex-col gap-7 pt-5 items-start">
+      <h1 className="headingL space-y-1">Maximize skill, minimize budget</h1>
+      <p className="bodyM">
+        Our modern courses across a range of in-demand skills will give you the
+        knowledge you need to live the life you want.
+      </p>
+      <HerosectionButton />
+      <img src={heroImageMobile} alt="hero-image" />
+    </div>
+  );
 };
---
+++ elearning-lp/src/components/MainContent/HerosectionButton.tsx
@@ -0,0 +1,7 @@
+export const HerosectionButton = () => {
+  return (
+    <button className="orange-gradient text-white rounded-full py-4 px-10 hover:opacity-60 transition-colors">
+      Get startet
+    </button>
+  );
+};
