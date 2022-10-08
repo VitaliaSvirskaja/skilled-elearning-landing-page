b65f661d39dbd11f18162622feb5d6dbf9bb2003
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-08 08:46:38 +0200

    clean up unused css

--- elearning-lp/src/App.tsx
+++ elearning-lp/src/App.tsx
@@ -1,11 +1,10 @@
-import "./App.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
 import { MainContent } from "./components/MainContent/MainContent";
 
 function App() {
   return (
-    <div className="App bg-gradient-to-b from-white to-gradient-background-blue">
+    <div className="bg-gradient-to-b from-white to-gradient-background-blue">
       <Navbar />
       <MainContent />
       <Footer />
